d6884b1c2ff523ff6c680f1bc434379a7a492826
DrabbyPage <35715195+DrabbyPage@users.noreply.github.com>
2018-12-03 01:44:07 -0500

    Pathfinding Update
    A Star had an infinite loop, replaced it with dijkstra pathfinding and it works, I plan on adding path smoothing and diagonal connections
    
    it is not implemented in moving for the characters yet

--- Cameron's Project/Assets/Scripts/GridScript.cs
+++ Cameron's Project/Assets/Scripts/GridScript.cs
@@ -49,7 +49,7 @@ public class GridScript : MonoBehaviour
             Vector2 testStart = new Vector2(-30.0f, -20.0f);
             Vector2 testEnd = new Vector2(0.0f, 0.0f);
 
-            testPath = GetPath(testStart, testEnd);
+            testPath = GetDijkstraPath(testStart, testEnd);
         }
     }
 
@@ -86,231 +86,174 @@ public class GridScript : MonoBehaviour
             grid[i].GetComponent<NodeScript>().MakeConnections();
         }
     }
-
-    List<GameObject> GetPath(Vector3 start, Vector3 end)
+    
+    List<GameObject> GetDijkstraPath(Vector3 start, Vector3 end)
     {
-        List<GameObject> path = new List<GameObject>();
+        GameObject startNode = FindClosestNode(start);
+        GameObject startEndNode = FindClosestNode(end);
 
+        Debug.Log(startNode.name);
+        Debug.Log(startEndNode.name);
+
+        // initialize the record for the start nodes
+        NodeRecord startRecord = new NodeRecord();
+        startRecord.node = startNode;
+        startRecord.costSoFar = 0;
+
+        //initialize the open and closed list
         List<NodeRecord> openList = new List<NodeRecord>();
         List<NodeRecord> closedList = new List<NodeRecord>();
-
-        GameObject startNode = FindClosestNode(start);
-        GameObject endNode = FindClosestNode(end);
-
-        NodeRecord startRecord = new NodeRecord();
-        NodeScript.Connection startConnection = new NodeScript.Connection();
-
-        startRecord.node = startNode;
-        startRecord.recordConnection = startConnection;
-        startRecord.estiTotalCost = EstimateCost(startNode, endNode);
 
         openList.Add(startRecord);
 
         NodeRecord currentNode = new NodeRecord();
 
-        Debug.Log(endNode.name);
-        Debug.Log(startNode.name);
+        List<NodeScript.Connection> connections = new List<NodeScript.Connection>();
 
-        return null;
-
-        //iterate through processing each node
-        while (openList.Count > 0)
+        // iterate through each process
+        while(openList.Count > 0)
         {
-            //find the smallest element in the open list
+            NodeRecord endNodeRecord = new NodeRecord();
+
+            // find the smallest element in the open list
             currentNode = openList[0];
 
-            // if it is the goal node then terminate
-            if (currentNode.node == endNode)
+            // if we are at the goal node then terminate
+            if(currentNode.node == startEndNode)
             {
-                Debug.Log("there is no route");
+                Debug.Log("at end of dijkstra");
                 break;
             }
-            // otherwise get its outgoing connections
+            // otherwise get the outgoing connections
             else
             {
-                // connections= graph.getConnections(current)
-                List<NodeScript.Connection> curNodeConnect = new List<NodeScript.Connection>();
-                curNodeConnect = currentNode.node.GetComponent<NodeScript>().GetConnections();
+                connections = openList[0].node.GetComponent<NodeScript>().GetConnections();
 
                 // loop through each connection in turn
-                for (int i = 0; i < curNodeConnect.Count; i++)
+                for(int i = 0; i < connections.Count; i++)
                 {
-                    // get the cost estimate for the end node
-                    GameObject newEndNode = curNodeConnect[i].toNode;
-                    float endNodeCost = currentNode.costSoFar + 1; // usually i would have to get the amount in
-                                                                   // connection but we didnt have a cost amount for
-                                                                   // connections so it is just 1
-
-                    NodeRecord endNodeRecord = new NodeRecord();
-                    float endNodeHeuristic;
+                    // get the cost estimate for teh end node
+                    GameObject newEndNode = connections[i].toNode;
+                    NodeScript.Connection newEndConnect = connections[i];
+                    float endNodeCost = currentNode.costSoFar + connections[i].weight;
 
                     bool inClosedList = false;
                     bool inOpenList = false;
 
-                    // if the node is closed we may have to skip or remove it from the close list
-                    //for (NodeRecord nodeRecord = closedList[0]; nodeRecord.node != closedList[closedList.Count-1].node; nodeRecord++)
-                    for (int j = 0; j < closedList.Count; j++)
+                    // skip if the node is closed
+                    for(int j = 0; j < closedList.Count; j++)
                     {
-                        NodeRecord newNodeRecord = closedList[j];
-
-                        // here we find the record in the closed list corresponding to the end node
-                        if (newNodeRecord.node == endNode)
+                        if(closedList[j].node == newEndNode)
                         {
                             inClosedList = true;
-                            endNodeRecord.node = newNodeRecord.node;
+                            break;
                         }
-
-                        // if we didnt find a shorter route, skip
-                        if (endNodeRecord.costSoFar <= endNodeCost)
-                        {
-                            // continue;
-                            continue;
-                        }
-                        // otherwise remove it from the closed list;
-                        else
-                        {
-                            closedList.Remove(newNodeRecord);
-
-                            // we can use the node's old cost values to calculate its heuristic without
-                            // calling the possibly expensive heuristic function
-                            endNodeHeuristic = endNodeRecord.estiTotalCost - endNodeRecord.costSoFar;
-                        }
-
-
                     }
-                    if (!inClosedList)
+                    //else if open has the end node
+                    if(!inClosedList)
                     {
-                        // skip if the node is open and we've not found a better route
-                        //for (auto record = openList.begin(); record != openList.end(); record++)
-                        for (int j = 0; j < openList.Count; j++)
+                        for(int j = 0; j < openList.Count; j++)
                         {
-                            NodeRecord newNodeRecord = openList[j];
-
-                            if (newNodeRecord.node == endNode)
+                            if(openList[j].node == newEndNode)
                             {
+                                // here we find the record in the oopen list corresponding to the endNode
+                                endNodeRecord.node = openList[j].node;
                                 inOpenList = true;
 
-                                // here we find the record in the open list corresponding to the endNode
-                                endNodeRecord.node = newNodeRecord.node;
 
-                                // if our route is no better, then skip
+                                // if(endNodeRecord.cost <= endNodeCost)
                                 if (endNodeRecord.costSoFar <= endNodeCost)
                                 {
                                     // continue
                                     continue;
                                 }
-                                else
-                                {
-
-                                    // we can use the node's old cost values to calculate its heuristic without
-                                    // calling the possibly expensive heuristic function
-                                    endNodeHeuristic = endNodeRecord.recordConnection.weight - currentNode.costSoFar;
-                                }
                                 break;
                             }
-
                         }
                     }
-                    // otherwise we know we've got an unvisited node so make a record for it
-                    if (!inClosedList && !inOpenList)
+                    if(!inOpenList && !inClosedList)
                     {
-                        endNodeRecord = new NodeRecord();
                         endNodeRecord.node = newEndNode;
 
-                        // we'll need to calculate the value using the function, since
-                        // we dont have an existing record to use
-                        endNodeHeuristic = EstimateCost(newEndNode, endNode);
-
-                        // we're here if we need to update the node
-                        // update the cost, estimate, and connection
-                        endNodeRecord.costSoFar = endNodeCost;
-                        endNodeRecord.recordConnection = curNodeConnect[i];
-                        endNodeRecord.estiTotalCost = endNodeCost + endNodeHeuristic;
-
+                        // we are here of we need to update the node
+                        // update the cost and connection
+                        endNodeRecord.recordConnection = newEndConnect;
                     }
 
-                    // and add it to the openList
-                    if (!inClosedList)
+                    // add it into the open list
+                    if(!inClosedList)
                     {
                         bool inOpen = false;
-                        //for (auto record = openList.begin(); record < openList.end(); record++)
-                        for (int j = 0; j < openList.Count; j++)
+                        for(int j = 0; j < openList.Count; j++)
                         {
-                            NodeRecord record = openList[j];
-
-                            if (record.node == endNodeRecord.node)
+                            if(openList[j].node == endNodeRecord.node)
                             {
                                 inOpen = true;
-                                break;
                             }
                         }
-
-                        if (!inOpen)
+                        if(!inOpen)
                         {
                             openList.Add(endNodeRecord);
                         }
                     }
                 }
-                // we've finished looking at the connections for the current node so add it to the closed 
-                // list and remove it from the open list
-                openList.RemoveAt(0);
-                closedList.Add(currentNode);
             }
+
+            // we have finished looking at the connections for the current node
+            // so add it to the closed list and remove it from the open list
+            openList.RemoveAt(0);
+            closedList.Add(currentNode);
         }
 
-
-        // we're here if we found a goal or if we have no more nodes to search find which one
-        if (currentNode.node != endNode)
+        // we are here if we have either found the goal or if we 
+        // have no more nodes to search, find which:
+        if(currentNode.node != startEndNode)
         {
-            // we've run out of nodes without finding the goal, so theres no solution
-            // return none
-            Debug.Log("did not end on goal node");
+            //we have run out of nodes without findign the goal so 
+            // there is no solution
+            Debug.Log("there is no solution");
+            canPressButton = true;
             return null;
         }
-        // else:
         else
         {
-            // compile the list of connections in the path
-            List<GameObject> a_Star_Path = new List<GameObject>();
+            List<GameObject> dijkstraPath = new List<GameObject>();
 
-            // work back along the path, accumulating connections
-            while (currentNode.node != startNode)
+            //work back along the path, accumulating connections
+            while(currentNode.node != startNode)
             {
-                // path+= current.connection
-                // current = current.connection.getFromNode()
-                // FYI Update the current's connection as well
-                a_Star_Path.Add(currentNode.node);
+                dijkstraPath.Add(currentNode.node);
                 currentNode.node = currentNode.recordConnection.fromNode;
 
-                //for (auto node = closedList.begin(); node < closedList.end(); node++)
-                for (int i = 0; i < closedList.Count; i++)
+                for(int i = 0; i < closedList.Count; i++)
                 {
-                    
-                    if (closedList[i].node == currentNode.node)
+                    if(closedList[i].node == currentNode.node)
                     {
                         currentNode.recordConnection = closedList[i].recordConnection;
                     }
-                    
                 }
             }
 
-            // reverse the path, and return it
+            // the path is backwards so we must reverse the path
             List<GameObject> reversePath = new List<GameObject>();
 
-            for (int i = 0; i < a_Star_Path.Count; i++)
+            for(int i = 0; i < dijkstraPath.Count; i++)
             {
                 GameObject newNode;
                 int lastNodeIndex;
 
-                lastNodeIndex = a_Star_Path.Count - (i + 1);
+                lastNodeIndex = dijkstraPath.Count - (i + 1);
 
-                newNode = a_Star_Path[lastNodeIndex];
+                newNode = dijkstraPath[lastNodeIndex];
 
                 reversePath.Add(newNode);
             }
 
-            path = reversePath;
-            return path;
+            Debug.Log("finished making the path");
+
+            canPressButton = true;
+
+            return reversePath;
         }
     }
 
